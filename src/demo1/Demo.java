58d9480b922a7f5c4f310d500be8eca4e9b57dfd
Jvaldeal <UO263764@uniovi.es>
2021-03-01 17:57:39 +0100

    cambios en fichero

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -2,4 +2,9 @@ package demo1;
 
 public class Demo {
 
+		public static void main(String[] args) {
+			System.out.println("linea1");
+			System.out.println("linea2");
+			System.out.println("linea3");
+		}
 }
